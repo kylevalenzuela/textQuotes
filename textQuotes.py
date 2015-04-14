42cbcd1db489286f7a380b740e2f8c510f443c5c
Kyle Valenzuela <valenzuela.kyle@gmail.com>
2015-04-14 16:00:12 -0700

    Same quote bug fixed

--- textQuotes.py
+++ textQuotes.py
@@ -1,23 +1,32 @@
-from twilio.rest import TwilioRestClient 
 import time
 import schedule
-import random
+import json
+import urllib
+from twilio.rest import TwilioRestClient
 
-ACCOUNT_SID = "" 
-AUTH_TOKEN = "" 
-client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
-now = time.strftime("%H:%M:%S")
+nums = [""]
+ACCOUNT_SID = ''
+AUTH_TOKEN = ''
 
-def job():
-	quotes = ["You fucking suck", "You are fat", "Tacos", "Pussy"]
-	re = random.choice(quotes)
-	client.messages.create(
-		to="+", 
-		from_="+", 
-		body=re, 
-	) 
 
-schedule.every().day.at("20:48").do(job)
+def textIt():
+	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+	url = "http://www.iheartquotes.com/api/v1/random?format=json&max_characters=160&source=hitchhiker+dune+plato+science+education+henry_david_thoreau+libery+wisdom+love+literature+technology+nietzsche+math+oscar_wilde+wisdom+humorists+art+platitudes"
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
+	quote = data["quote"]
+	for n in nums: 
+		client.messages.create(
+			to=n, 
+			from_="", 
+			body=quote 
+		) 
+
+
+schedule.every().day.at("10:00").do(textIt)
+schedule.every().day.at("13:00").do(textIt)
+schedule.every().day.at("16:00").do(textIt)
+schedule.every().day.at("20:00").do(textIt)
 
 while True:
     schedule.run_pending()
